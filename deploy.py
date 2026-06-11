a144037ef5b9bb6976dc583993e09b2dd47444b3
bsudarshan7 <161239316+bsudarshan7@users.noreply.github.com>
2026-06-11 18:50:38 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,7 +1,7 @@
 import paramiko
 
 HOST = "35.154.77.244"
-USERNAME = "ubuntu"
+USERNAME = "ec2-user"
 KEY_FILE = "my-key.pem"
 
 ssh = paramiko.SSHClient()
@@ -15,10 +15,22 @@ ssh.connect(
 
 sftp = ssh.open_sftp()
 
-sftp.put("main1.py", "/home/ubuntu/main1.py")
+sftp.put("main1.py", "/home/ec2-user/main1.py")
 
-print("main1.py uploaded successfully!")
+print("File uploaded successfully!")
 
 sftp.close()
+
+stdin, stdout, stderr = ssh.exec_command(
+    "python3 /home/ec2-user/main1.py"
+)
+
+print("Program Output:")
+print(stdout.read().decode())
+
+error = stderr.read().decode()
+if error:
+    print("Errors:")
+    print(error)
+
 ssh.close()
-  
